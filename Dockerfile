f9c00023bf6bcb9301f427de82b34d53f667582b
ezpzlmnsqz1337 <zden.braun@seznam.cz>
2025-09-14 19:44:03 +0200

    chore: add Dockerfile and implement deploy scripts

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,15 @@
-FROM node:22 AS build
+FROM node:22-slim AS build
 
 WORKDIR /app
 
 COPY . /app
 
 RUN npm ci && npm run build
+RUN cd multiplayer-server && npm ci && npm run build
 
-FROM nginx
-COPY --from=build /app/multiplayer-server/dist /usr/share/nginx/html
+FROM node:22-slim
+
+WORKDIR /app
+
+COPY --from=build /app/multiplayer-server /app
+ENTRYPOINT [ "node", "/app/build/index.js" ]
--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@
 shopt -s expand_aliases
 DESTINATION="hosting@hosting"
 
-podman build --pull --rm -f 'Dockerfile' -t 'jumping-game-ui:latest' '.'
-podman save jumping-game-ui:latest -o jumping-game-ui.tar
+podman build --pull --rm -f 'Dockerfile' -t 'jumping-game:latest' '.'
+podman save jumping-game:latest -o jumping-game.tar
 
-scp -r jumping-game-ui.tar $DESTINATION:/opt/containers/jumping-game-ui.tar
+scp -r jumping-game.tar $DESTINATION:/opt/containers/jumping-game.tar
 
-ssh $DESTINATION 'podman load -i /opt/containers/jumping-game-ui.tar'
+ssh $DESTINATION 'podman load -i /opt/containers/jumping-game.tar'
 
-ssh $DESTINATION 'systemctl --user restart container-jumping-game-ui'
+ssh $DESTINATION 'systemctl --user restart container-jumping-game'
